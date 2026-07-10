a91426167683e3bbd0e958773157c8931859ec2e
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-07-10 18:22:53 +0530

    new changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,44 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "2634282";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(l) {
+                if (!l) {
+                  window.lintrk = function(a, b) { window.lintrk.q.push([a, b]); };
+                  window.lintrk.q = [];
+                }
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";
+                b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=2634282&fmt=gif"
+          />
+        </noscript>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
